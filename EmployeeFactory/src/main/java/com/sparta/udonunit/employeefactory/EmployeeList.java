d2b98c55fff76e6f7f4388390243b7a49baaa28a
moonraghib97 <moonraghib97@gmail.com>
2024-04-24 00:00:55 +0100

    Added EmployeeList object coverter class

---
+++ EmployeeFactory/src/main/java/com/sparta/udonunit/employeefactory/EmployeeList.java
@@ -0,0 +1,37 @@
+package com.sparta.udonunit.employeefactory;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmployeeList {
+    List<Employee> employees = new ArrayList<>();
+
+    public EmployeeList(){
+    }
+
+    public List<Employee> convertEmployeeDataListIntoObjects(String[] employeeData) {
+
+        for (String data : employeeData) {
+            String[] values = data.split(",");
+            
+            int empId = Integer.parseInt(values[0]);
+            String prefix = values[1];
+            String firstName = values[2];
+            String middleInitial = values[3];
+            String lastName = values[4];
+            String gender = values[5];
+            String email = values[6];
+            LocalDate dob = LocalDate.parse(values[7]);
+            LocalDate hireDate = LocalDate.parse(values[8]);
+            int salary = Integer.parseInt(values[9]);
+
+            Employee employee = new Employee(empId, prefix, firstName, middleInitial, lastName,
+                    gender, email, dob, hireDate, salary);
+            employees.add(employee);
+        }
+
+        return employees;
+    }
+}
+
--- EmployeeFactory/src/main/java/com/sparta/udonunit/employeefactory/Main.java
+++ EmployeeFactory/src/main/java/com/sparta/udonunit/employeefactory/Main.java
@@ -1,9 +1,12 @@
 package com.sparta.udonunit.employeefactory;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Arrays.asList(EmployeeFactory.getEmployees(20)));
+        //System.out.println(Arrays.asList(EmployeeFactory.getEmployees(20)));
+
+
     }
 }
