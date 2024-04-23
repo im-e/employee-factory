4d56c5c35cd1b26deb35cc0bac41011be639530c
moonraghib97 <moonraghib97@gmail.com>
2024-04-23 13:53:03 +0100

    added to DTO class

--- EmployeeFactory/src/main/java/com/sparta/udonunit/employeefactory/DTO.java
+++ EmployeeFactory/src/main/java/com/sparta/udonunit/employeefactory/DTO.java
@@ -1,20 +1,22 @@
 package com.sparta.udonunit.employeefactory;
 
-public class DTO {
-    private int empId;
-    private String title;
-    private String firstName;
-    private String middleInitial;
-    private String lastName;
-    private String gender;
-    private String email;
-    private String dob; // Date of Birth
-    private String hireDate;
-    private int salary;
+import java.time.LocalDate;
 
-    // Constructor, getters, and setters
-    // Constructor
-    public DTO(int empId, String title, String firstName, String middleInitial, String lastName, String gender, String email, String dob, String hireDate, int salary) {
+public class DTO {
+    private final int empId;
+    private final String title;
+    private final String firstName;
+    private final String middleInitial;
+    private final String lastName;
+    private final String gender;
+    private final String email;
+    private final LocalDate dob; // Date of Birth
+    private final LocalDate hireDate;
+    private final int salary;
+
+
+    public DTO(int empId, String title, String firstName, String middleInitial, String lastName, String gender,
+               String email, LocalDate dob, LocalDate hireDate, int salary) {
         this.empId = empId;
         this.title = title;
         this.firstName = firstName;
@@ -27,6 +29,59 @@ public class DTO {
         this.salary = salary;
     }
 
-    // Getters and Setters
-    // ...
+    public int getEmpId() {
+        return empId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getMiddleInitial() {
+        return middleInitial;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public LocalDate getHireDate() {
+        return hireDate;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    @Override
+    public String toString() {
+        return "DTO{" +
+                "empId=" + empId +
+                ", title='" + title + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", middleInitial='" + middleInitial + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", gender='" + gender + '\'' +
+                ", email='" + email + '\'' +
+                ", dob=" + dob +
+                ", hireDate=" + hireDate +
+                ", salary=" + salary +
+                '}';
+    }
 }
