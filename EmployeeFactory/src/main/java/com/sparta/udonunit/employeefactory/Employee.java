705796fdb5ade4baeac42e3feec60dbc17273bda
moonraghib97 <moonraghib97@gmail.com>
2024-04-24 09:25:11 +0100

    modified EmployeeList object coverter class

--- EmployeeFactory/src/main/java/com/sparta/udonunit/employeefactory/Employee.java
+++ EmployeeFactory/src/main/java/com/sparta/udonunit/employeefactory/Employee.java
@@ -2,73 +2,11 @@ package com.sparta.udonunit.employeefactory;
 
 import java.time.LocalDate;
 
-public class Employee {
-
-    private final int empId;
-    private final String title;
-    private final String firstName;
-    private final String middleInitial;
-    private final String lastName;
-    private final String gender;
-    private final String email;
-    private final LocalDate dob; // Date of Birth
-    private final LocalDate hireDate;
-    private final int salary;
-
-
-    public Employee(int empId, String title, String firstName, String middleInitial, String lastName, String gender,
-                    String email, LocalDate dob, LocalDate hireDate, int salary) {
-        this.empId = empId;
-        this.title = title;
-        this.firstName = firstName;
-        this.middleInitial = middleInitial;
-        this.lastName = lastName;
-        this.gender = gender;
-        this.email = email;
-        this.dob = dob;
-        this.hireDate = hireDate;
-        this.salary = salary;
-    }
-
-    public int getEmpId() {
-        return empId;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getMiddleInitial() {
-        return middleInitial;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public LocalDate getDob() {
-        return dob;
-    }
-
-    public LocalDate getHireDate() {
-        return hireDate;
-    }
-
-    public int getSalary() {
-        return salary;
-    }
+/**
+ * @param dob Date of Birth
+ */
+public record Employee(int empId, String title, String firstName, String middleInitial, String lastName, String gender,
+                       String email, LocalDate dob, LocalDate hireDate, int salary) {
 
     @Override
     public String toString() {
--- EmployeeFactory/src/main/java/com/sparta/udonunit/employeefactory/EmployeeList.java
+++ EmployeeFactory/src/main/java/com/sparta/udonunit/employeefactory/EmployeeList.java
@@ -1,6 +1,7 @@
 package com.sparta.udonunit.employeefactory;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,7 +15,7 @@ public class EmployeeList {
 
         for (String data : employeeData) {
             String[] values = data.split(",");
-            
+
             int empId = Integer.parseInt(values[0]);
             String prefix = values[1];
             String firstName = values[2];
@@ -22,8 +23,8 @@ public class EmployeeList {
             String lastName = values[4];
             String gender = values[5];
             String email = values[6];
-            LocalDate dob = LocalDate.parse(values[7]);
-            LocalDate hireDate = LocalDate.parse(values[8]);
+            LocalDate dob = LocalDate.parse(values[7], DateTimeFormatter.ofPattern("MM/dd/yyyy"));
+            LocalDate hireDate = LocalDate.parse(values[8], DateTimeFormatter.ofPattern("MM/dd/yyyy"));
             int salary = Integer.parseInt(values[9]);
 
             Employee employee = new Employee(empId, prefix, firstName, middleInitial, lastName,
